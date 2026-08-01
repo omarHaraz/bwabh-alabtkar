d95dbeec439b272b2c892e80fecd38f3450e953d
omarHaraz <omarharaz553@gmail.com>
2026-08-01 23:05:04 +0300

    Move frontend to docs folder and add root redirect for GitHub Pages

--- frontend/auth/services/AuthService.js
+++ frontend/auth/services/AuthService.js
@@ -1,6 +1,6 @@
 import axios from "https://cdn.skypack.dev/axios";
 
-const API_URL = "http://localhost:8080/api/auth/";
+const API_URL = "https://bwabh-alabtkar-com.onrender.com/api/auth/";
 
 class AuthService {
 
